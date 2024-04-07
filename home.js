5d5320d8db606b57c815e706c85520884e4ccda8
Miftah Muhammad Farhan <miftahfarhan20@gmail.com>
2024-04-07 14:00:51 +0700

    add changes home js

--- home.js
+++ home.js
@@ -1,1 +1,7 @@
 const name = "latihan git"
+
+// accept current change - accept incoming change - accept both - compare changes
+const age = 30 //codingan ku
+const address = "home" //codingan mas rizky
+
+//git stash - git pull - restore stash
